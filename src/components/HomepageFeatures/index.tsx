bbf14f5587edda01566fe8b1a674221c817778eb
申栋栋 <sailwebs@infracreate.com>
2022-08-25 19:26:37 +0800

    feat: dark mode images

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -13,22 +13,22 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Ready for more complex application scenarios',
-    img: '230.jpeg',
+    img: '230.png',
     description: 'Kubelego is ready to meet your requirements for more complex application scenarios, aiming to solving the technical and costs challenges you are faced with.',
   },
   {
     title: 'Open source and neutral',
-    img: '231.jpeg',
+    img: '231.png',
     description: 'Although current cloud vendors have public data management platforms, they only support a few mainstream databases. Third-party proprietary open source licenses are never fully-supported, because in essence cloud vendors cannot achieve open source and be neutral.',
   },
   {
     title: 'The requirement of multi-cloud and hybrid cloud management',
-    img: '232.jpeg',
+    img: '232.png',
     description: 'You may use not single cloud platforms (AWS, Axure, Google Cloud) and with self-built cloud platforms (hybrid cloud). And cloud vendors cannot be neutral about open source software. Kubelego provides an easy way for database management and long-term maintenance.',
   },
   {
     title: 'The chronic problem of data silo',
-    img: '233.jpeg',
+    img: '233.png',
     description: 'Kubelego is a unified data management interface. Under a unified management interface, effective unification can be achieved off-line. This is a problem that the current data lake and data warehouse products can not effectively solve, and the data lake itself is another relatively large data silo.',
   },
 ];
