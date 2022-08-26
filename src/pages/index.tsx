5575d01da96ff71021977d370b20fbb55bce7001
申栋栋 <sailwebs@infracreate.com>
2022-08-26 12:37:10 +0800

    feat: mobile support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,27 +41,31 @@ export default function Home(): JSX.Element {
       <header className={styles.banner}>
         <div className="container">
           <div className={styles.inner}>
-              <QueueAnim className={styles.left} duration={500} type={['left', 'right']}>
-                <h1 key={1}>The Database of next generation</h1>
-                <h2 key={2}>Build your database as blocks</h2>
-                <div key={3} className={styles.actions}>
-                  <Link
-                    className={styles.actionItem}
-                    to="/docs/overview">
-                    Get Started
-                  </Link>
-                  <a target="_blank" className={styles.actionItem} href="https://github.com/apecloud/">GitHub <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>
-                </div>
-              </QueueAnim>
- 
-            <QueueAnim className={styles.right} duration={1000} />
+            <QueueAnim className={styles.left} duration={500} type={['left', 'right']}>
+              <h1 key={1}>The Database of next generation</h1>
+              <h2 key={2}>Build your database as blocks</h2>
+              <div key={3} className={styles.actions}>
+                <Link
+                  className={styles.actionItem}
+                  to="/docs/overview">
+                  Get Started
+                </Link>
+                <a target="_blank" className={styles.actionItem} href="https://github.com/apecloud/">GitHub <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg></a>
+              </div>
+            </QueueAnim>
+
+            <QueueAnim duration={1000}>
+              <div key={1} className={styles.right}>
+                <img src="img/banner.png" />
+              </div>
+            </QueueAnim>
           </div>
         </div>
       </header>
       <main>
-      <div className="container">
-        <div className={styles.features}>
-          {FeatureList.map(({ title, img, description }, idx) => (
+        <div className="container">
+          <div className={styles.features}>
+            {FeatureList.map(({ title, img, description }, idx) => (
               <ScrollParallax className={styles.item} key={idx} animation={{ left: -40 }}>
                 <div className={styles.img} ><img src={`img/${img}`} /></div>
                 <div className={styles.content}>
