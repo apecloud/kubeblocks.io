ba919eca843e8159370d211846b393a46b8d9eec
zhouxinyi@apecloud.com <zhouxinyi@apecloud.com>
2024-01-04 16:38:13 +0800

    feat: kubechat

--- src/components/docItemComponent.tsx
+++ src/components/docItemComponent.tsx
@@ -1,48 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from "react"
 import DocItem from "@theme/DocItem";
 import BackToTopButton from "@theme/BackToTopButton";
 import DocSidebar from "@theme/DocSidebar";
 
-import KubeChat from '../components/kubechat';
-import classNames from 'classnames';
-import styles from '../pages/index.module.less';
-
 export default (props) => {
-
-  const [open, setOpen] = useState<boolean>(false);
-  const [chatReady, setChatReady] = useState<boolean>(false);
 
   return (
     <>
       <DocItem route={props.route} content={props.content} />
-      <div>
-        <BackToTopButton />
-        <div
-          className={classNames({
-            [styles.kubechat]: true,
-            [styles.kubechatOpen]: open,
-            [styles.kubechatReady]: chatReady
-          })}
-        >
-          <KubeChat
-            onOpen={() => {
-              setOpen(true);
-            }}
-            onClose={() => {
-              setOpen(false);
-            }}
-            onReady={() => {
-              setChatReady(true);
-            }}
-          />
-
-        </div>
-      </div>
-
-
-
-
-
+      <BackToTopButton />
     </>
   )
 }
