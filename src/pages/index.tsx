4918b839cc6da810d6c2641c4ea6fe7f4febabde
zhouxinyi@apecloud.com <zhouxinyi@apecloud.com>
2024-01-19 11:57:42 +0800

    feat: css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,13 +116,13 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <div className={styles.bg}>
-        <a href='https://github.com/apecloud/kubeblocks' target='_blank' className={styles.git}>
+        {/* <a href='https://github.com/apecloud/kubeblocks' target='_blank' className={styles.git}>
           <span className={styles.text}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
               <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
             </svg>
             Star us on GitHub</span>
-        </a>
+        </a> */}
         <div className={styles.home}>
           <div className={styles.banner}>
             <QueueAnim duration={500} type={['left', 'right']}>
