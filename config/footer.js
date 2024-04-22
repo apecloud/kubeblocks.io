69d5cb4c76e18d769c175340255d9cd528fea7f0
yuanyuan zhang <yuanyuan.zhang@apecloud.com>
2024-04-22 14:01:31 +0800

    chore: update slack link

--- config/footer.js
+++ config/footer.js
@@ -68,7 +68,7 @@ module.exports = {
         {
           html: `
               <span class="contact-span">
-                <a href="https://join.slack.com/t/kubeblocks/shared_invite/zt-2f9hztwcg-hcos~WYoDqXjitobfGMTgQ" target="_blank" rel="noreferrer noopener">
+                <a href="https://join.slack.com/t/kubeblocks/shared_invite/zt-2hgvigyfl-~PB8vwXqQV1v7uur~qFaxQ" target="_blank" rel="noreferrer noopener">
                   Slack
                 </a>
               </span>
